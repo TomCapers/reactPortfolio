c13307220cef126be9a5606ea37bee571d021e63
Tom Capers <tompcapers@gmail.com>
2021-08-23 16:10:26 -0700

    updated flex on wrapper

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ function App() {
    
       <div>
       <Header/>
-      <Wrapper>
+    
         <div id="about-me">
         <h2>About Me</h2>
           <p>Experienced financial services professional and UCLA Coding Boot Camp graduate motivated to build a career in the web development industry. Highly organized, results oriented professional with talent for balancing production needs with customer service and efficiency. Recognized for ability to lead and guide operating units while increasing technical expertise, skills and knowledge of staff. Excels at learning and applying new skills in fast paced and demanding environments. Strong team player that is also able to work independently on projects.</p>
         </div>
         <h2>Recent Projects</h2>
+
+        <Wrapper>
       <Project
         name={projects[0].name}
         image={projects[0].image}
